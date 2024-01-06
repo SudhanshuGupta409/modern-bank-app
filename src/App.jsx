b2ebcff8ca47795708938edbab7f0a34f9b84d84
Sudhanshu Gupta <sudhanshugupta409@gamil.com>
2024-01-06 12:17:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./style";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth} `}>NavBar</div>
+    </div>
+  </div>
+);
+
+export default App;
