66adf386dd08a16ba2fdbac89327b334a78eb691
Sudhanshu Gupta <sudhanshugupta409@gamil.com>
2024-01-07 11:28:05 +0530

    Implement Stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,24 @@
+import { stats } from "../constants";
+import styles from "../style";
+
 function Stats() {
-  return <div>Stats</div>;
+  return (
+    <div className={`${styles.flexCenter} flex-row flex-wrap md:mb-20 mb-6`}>
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className={`flex flex-1 justify-start items-center flex-row m-3`}
+        >
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Stats;
